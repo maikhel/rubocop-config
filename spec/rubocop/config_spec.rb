c55e0398fa51f6d203666829f767388ad6948c43
Michal Lecicki <mlecicki@fundthrough.com>
2021-05-26 12:18:16 +0200

    Create gem

---
+++ spec/rubocop/config_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rubocop::Config do
+  it "has a version number" do
+    expect(Rubocop::Config::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
